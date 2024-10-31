c3c59e8d60168a4fee17b844e49ec8c66a366bfb
Aia <pai2me216@gmail.com>
2024-10-31 18:58:58 -0300

    Implementations

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,15 +1,19 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
     execute(url: string): Promise<boolean>
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
-    ) {}
+    ) { }
 
     public async execute(url: string): Promise<boolean> {
         try {
@@ -17,12 +21,19 @@ export class CheckService implements CheckServiceUseCase {
             if (!req.ok) {
                 throw new Error(`Error on check ${url}`);
             }
-            this.successCallback();
-            return true
+
+            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+            this.logRepository.saveLog(log);
+            this.successCallback && this.successCallback();
+
+            return true;
+
         } catch (error) {
-            console.log(`${error}`);
-            this.errorCallback(`${error}`);
-            return false
+            const errorMessage = `${url} is not ok. ${error}`;
+            const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+            this.logRepository.saveLog(log);
+            this.errorCallback && this.errorCallback(errorMessage);
+            return false;
         }
     }
 }
